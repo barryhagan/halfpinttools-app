2f33fb58e88d0a9b0bd3312edda905a258829c67
Barry Hagan <barryhagan@gmail.com>
2020-02-19 11:30:20 -0600

    Updates

---
+++ precache-manifest.16ced48609c9ec95a34819ffb63add42.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac2edb1a5e17792a0ec149ee8ef2e92d",
+    "url": "/halfpinttools-app/index.html"
+  },
+  {
+    "revision": "e9df4e49b2522371c46a",
+    "url": "/halfpinttools-app/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "8f1097adf241f5cccb78",
+    "url": "/halfpinttools-app/static/js/2.d5c98c7f.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/halfpinttools-app/static/js/2.d5c98c7f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9df4e49b2522371c46a",
+    "url": "/halfpinttools-app/static/js/main.0c2d19ec.chunk.js"
+  },
+  {
+    "revision": "2d330142a4e080b3bcb2",
+    "url": "/halfpinttools-app/static/js/runtime-main.a3aae906.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/halfpinttools-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
